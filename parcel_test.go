980c681518f963407dea27ec0e2adf5f1344a39b
Anna25-go <sportani4@gmail.com>
2026-02-03 14:04:53 +0400

    second

--- parcel_test.go
+++ parcel_test.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"math/rand"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	_ "modernc.org/sqlite"
 )
 
@@ -34,10 +35,7 @@ func TestAddGetDelete(t *testing.T) {
 	// prepare
 	// настройте подключение к БД
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	require.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -46,52 +44,28 @@ func TestAddGetDelete(t *testing.T) {
 	// add
 	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
 	number, err := store.Add(parcel)
-	if err != nil {
-		t.Errorf("Error adding: %v", err)
-	}
-	if number <= 0 {
-		t.Errorf("Expected positive parcel number, got: %d", number)
-	}
+	require.NoError(t, err, "Error adding: %v", err)
+	assert.Greater(t, number, 0, "Expected positive parcel number, got: %d", number)
 
 	// get
 	// получите только что добавленную посылку, убедитесь в отсутствии ошибки
 	// проверьте, что значения всех полей в полученном объекте совпадают со значениями полей в переменной parcel
 	parc, err := store.Get(number)
-	if err != nil {
-		t.Errorf("Error getting: %v", err)
-	}
-	if parc.Number != number {
-		t.Errorf("Parcel number mismatch. expected: %d, got: %d", number, parc.Number)
-	}
-
-	if parc.Client != parcel.Client {
-		t.Errorf("Client mismatch. expected: %d, got: %d", parcel.Client, parc.Client)
-	}
-
-	if parc.Status != parcel.Status {
-		t.Errorf("Status mismatch. expected: %s, got: %s", parcel.Status, parc.Status)
-	}
-
-	if parc.Address != parcel.Address {
-		t.Errorf("Address mismatch. expected: %s, got: %s", parcel.Address, parc.Address)
-	}
-
-	if parc.CreatedAt != parcel.CreatedAt {
-		t.Errorf("CreatedAt mismatch. expected: %s, got: %s", parcel.CreatedAt, parc.CreatedAt)
-	}
+	require.NoError(t, err, "Error getting: %v", err)
+	assert.Equal(t, number, parc.Number, "Parcel number mismatch. expected: %d, got: %d", number, parc.Number)
+	assert.Equal(t, parcel.Client, parc.Client, "Client mismatch. expected: %d, got: %d", parcel.Client, parc.Client)
+	assert.Equal(t, parcel.Status, parc.Status, "Status mismatch. expected: %s, got: %s", parcel.Status, parc.Status)
+	assert.Equal(t, parcel.Address, parc.Address, "Address mismatch. expected: %s, got: %s", parcel.Address, parc.Address)
+	assert.Equal(t, parcel.CreatedAt, parc.CreatedAt, "CreatedAt mismatch. expected: %s, got: %s", parcel.CreatedAt, parc.CreatedAt)
 
 	// delete
 	// удалите добавленную посылку, убедитесь в отсутствии ошибки
 	// проверьте, что посылку больше нельзя получить из БД
 	err = store.Delete(number)
-	if err != nil {
-		t.Errorf("Error deleting: %v", err)
-	}
+	require.NoError(t, err, "Error deleting: %v", err)
 
 	_, err = store.Get(number)
-	if err == nil {
-		t.Errorf("Expected error when getting deleted parcel, got nil")
-	}
+	assert.Error(t, err, "Expected error when getting deleted parcel, got nil")
 }
 
 // TestSetAddress проверяет обновление адреса
@@ -99,10 +73,7 @@ func TestSetAddress(t *testing.T) {
 	// prepare
 	// настройте подключение к БД
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	require.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -111,30 +82,20 @@ func TestSetAddress(t *testing.T) {
 	// add
 	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
 	number, err := store.Add(parcel)
-	if err != nil {
-		t.Errorf("Error adding: %v", err)
-	}
-	if number <= 0 {
-		t.Errorf("Expected positive parcel number, got: %d", number)
-	}
+	require.NoError(t, err, "Error adding: %v", err)
+	assert.Greater(t, number, 0, "Expected positive parcel number, got: %d", number)
 
 	// set address
 	// обновите адрес, убедитесь в отсутствии ошибки
 	newAddress := "new test address"
 	err = store.SetAddress(number, newAddress)
-	if err != nil {
-		t.Errorf("Error setting address: %v", err)
-	}
+	require.NoError(t, err, "Error setting address: %v", err)
 
 	// check
 	// получите добавленную посылку и убедитесь, что адрес обновился
 	parc, err := store.Get(number)
-	if err != nil {
-		t.Errorf("Error getting: %v", err)
-	}
-	if parc.Address != newAddress {
-		t.Errorf("Address mismatch. expected: %s, got: %s", newAddress, parc.Address)
-	}
+	require.NoError(t, err, "Error getting: %v", err)
+	assert.Equal(t, newAddress, parc.Address, "Address mismatch. expected: %s, got: %s", newAddress, parc.Address)
 }
 
 // TestSetStatus проверяет обновление статуса
@@ -142,10 +103,7 @@ func TestSetStatus(t *testing.T) {
 	// prepare
 	// настройте подключение к БД
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	require.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -154,30 +112,20 @@ func TestSetStatus(t *testing.T) {
 	// add
 	// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
 	number, err := store.Add(parcel)
-	if err != nil {
-		t.Errorf("Error adding: %v", err)
-	}
-	if number <= 0 {
-		t.Errorf("Expected positive parcel number, got: %d", number)
-	}
+	require.NoError(t, err, "Error adding: %v", err)
+	assert.Greater(t, number, 0, "Expected positive parcel number, got: %d", number)
 
 	// set status
 	// обновите статус, убедитесь в отсутствии ошибки
 	status := "registered"
 	err = store.SetStatus(number, status)
-	if err != nil {
-		t.Errorf("Error setting status: %v", err)
-	}
+	require.NoError(t, err, "Error setting status: %v", err)
 
 	// check
 	// получите добавленную посылку и убедитесь, что статус обновился
 	parc, err := store.Get(number)
-	if err != nil {
-		t.Errorf("Error getting: %v", err)
-	}
-	if parc.Status != status {
-		t.Errorf("Status mismatch. expected: %s, got: %s", status, parc.Status)
-	}
+	require.NoError(t, err, "Error getting: %v", err)
+	assert.Equal(t, status, parc.Status, "Status mismatch. expected: %s, got: %s", status, parc.Status)
 }
 
 // TestGetByClient проверяет получение посылок по идентификатору клиента
@@ -185,10 +133,7 @@ func TestGetByClient(t *testing.T) {
 	// prepare
 	// настройте подключение к БД
 	db, err := sql.Open("sqlite", "tracker.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	require.NoError(t, err)
 	defer db.Close()
 
 	store := NewParcelStore(db)
@@ -210,12 +155,8 @@ func TestGetByClient(t *testing.T) {
 	for i := 0; i < len(parcels); i++ {
 		// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
 		id, err := store.Add(parcels[i])
-		if err != nil {
-			t.Errorf("Error adding: %v", err)
-		}
-		if id <= 0 {
-			t.Errorf("Expected positive parcel number, got: %d", id)
-		}
+		require.NoError(t, err, "Error adding: %v", err)
+		assert.Greater(t, id, 0, "Expected positive parcel number, got: %d", id)
 
 		// обновляем идентификатор добавленной у посылки
 		parcels[i].Number = id
@@ -229,12 +170,8 @@ func TestGetByClient(t *testing.T) {
 	// убедитесь в отсутствии ошибки
 	// убедитесь, что количество полученных посылок совпадает с количеством добавленных
 	storedParcels, err := store.GetByClient(client)
-	if err != nil {
-		t.Errorf("Error getting by client: %v", err)
-	}
-	if len(storedParcels) != len(parcels) {
-		t.Errorf("Getting by client mismatch. expected: %d, got: %d", len(parcels), len(storedParcels))
-	}
+	require.NoError(t, err, "Error getting by client: %v", err)
+	assert.Len(t, storedParcels, len(parcels), "Getting by client mismatch. expected: %d, got: %d", len(parcels), len(storedParcels))
 
 	// check
 	for _, parcel := range storedParcels {
@@ -242,36 +179,14 @@ func TestGetByClient(t *testing.T) {
 		// убедитесь, что все посылки из storedParcels есть в parcelMap
 		// убедитесь, что значения полей полученных посылок заполнены верно
 		originalParcel, exists := parcelMap[parcel.Number]
-		if !exists {
-			t.Errorf("parcel with number %d not found in original map", parcel.Number)
-			continue
-		}
+		assert.True(t, exists, "Parcel with number %d not found in original map", parcel.Number)
 
 		// Проверяем значения полей
-		if parcel.Client != originalParcel.Client {
-			t.Errorf("parcel %d: client mismatch. expected %d, got %d",
-				parcel.Number, originalParcel.Client, parcel.Client)
-		}
+		assert.Equal(t, client, parcel.Client, "Parcel %d: wrong client. expected %d, got %d", parcel.Number, client, parcel.Client)
+		assert.Equal(t, originalParcel.Client, parcel.Client, "Parcel %d: client mismatch. expected %d, got %d", parcel.Number, originalParcel.Client, parcel.Client)
+		assert.Equal(t, originalParcel.Status, parcel.Status, "Parcel %d: status mismatch. expected %s, got %s", parcel.Number, originalParcel.Status, parcel.Status)
+		assert.Equal(t, originalParcel.Address, parcel.Address, "Parcel %d: status mismatch. expected %s, got %s", parcel.Number, originalParcel.Address, parcel.Address)
+		assert.Equal(t, originalParcel.CreatedAt, parcel.CreatedAt, "Parcel %d: status mismatch. expected %s, got %s", parcel.Number, originalParcel.CreatedAt, parcel.CreatedAt)
 
-		if parcel.Status != originalParcel.Status {
-			t.Errorf("parcel %d: status mismatch. expected %s, got %s",
-				parcel.Number, originalParcel.Status, parcel.Status)
-		}
-
-		if parcel.Address != originalParcel.Address {
-			t.Errorf("parcel %d: address mismatch. expected %s, got %s",
-				parcel.Number, originalParcel.Address, parcel.Address)
-		}
-
-		if parcel.Address != originalParcel.Address {
-			t.Errorf("parcel %d: address mismatch. expected %s, got %s",
-				parcel.Number, originalParcel.Address, parcel.Address)
-		}
-
-		// Проверяем, что это посылка нужного клиента
-		if parcel.Client != client {
-			t.Errorf("parcel %d: wrong client. expected %d, got %d",
-				parcel.Number, client, parcel.Client)
-		}
 	}
 }
